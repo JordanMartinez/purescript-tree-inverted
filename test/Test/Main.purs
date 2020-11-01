ea420beef451fb4f4f46b6ecd2e5090734258174
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-10-31 18:15:46 -0700

    Write more tests; found bug in siblingIndices

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -10,7 +10,7 @@ import Test.Spec (describe, it, pending)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (runSpec)
-import Tree.InvertedTable.Internal (buildTree, rootIndex, siblingIndices, singleton)
+import Tree.InvertedTable.Internal (buildTree, pushChild, rootIndex, siblingIndices, singleton)
 
 nilInt :: Array Int
 nilInt = []
@@ -32,13 +32,16 @@ main = launchAff_ $ runSpec [consoleReporter ] do
             rootIndex rootOnlyTree `shouldEqual` 0
           it "should have 0 siblings" do
             (unsafePartial $ siblingIndices 0 rootOnlyTree) `shouldEqual` nilInt
-      describe "buildTree" do
-        it "creates correctly" do
-          let rootOnlyTree = buildTree "a" Nothing
-          pure unit
-        describe "indices are correct" do
-          let rootOnlyTree = buildTree "a" Nothing
-          it "root index refers to itself" do
-            rootIndex rootOnlyTree `shouldEqual` 0
-          it "should have 0 siblings" do
-            (unsafePartial $ siblingIndices 0 rootOnlyTree) `shouldEqual` nilInt
+    describe "buildTree" do
+      it "creates correctly" do
+        let abTree = buildTree "a" (pushChild "b" *> pushChild "c")
+        pure unit
+      let abTree = buildTree "a" (pushChild "b" *> pushChild "c")
+      describe "root indices are correct" do
+        it "root index refers to itself" do
+          rootIndex abTree `shouldEqual` 0
+        it "root should have 0 siblings" do
+          (unsafePartial $ siblingIndices 0 abTree) `shouldEqual` nilInt
+      describe "first child indices are correct" do
+        it "first child should have 1 sibling" do
+          (unsafePartial $ siblingIndices 1 abTree) `shouldEqual` [2]
